800b4cc0c90d45376df83cda00b8464a800c2e1d
v2538n <v2538n@gmail.com>
2020-11-12 15:54:57 +0300

    v3

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 (function($){
 	$(document).ready(function(){
 
-/* ----------------------------------------------------------------------------------------- */
-
-	
-	/* Анимация для вернхнего меню, при скролле страницы */
 
 		function scrollAnimation(){
 
@@ -16,26 +12,35 @@
 			const startPoint = $('.s2').offset().top;
 			const stopPoint  = ($('.s3').offset().top) - winHeight;
 			
-			const images = $('.content-img-fixed');
-			const image1 = startPoint;
-			const image2 = image1 + winHeight;
-			const image3 = image2 + winHeight;
-			const image4 = image3 + winHeight;
-			const image5 = image4 + winHeight;
-			const image6 = image5 + winHeight;
-			const image7 = image6 + winHeight;
-			const image8 = image7 + winHeight;
-			const image9 = image8 + winHeight;
-			const image10 = image9 + winHeight;
+			const images      = $('.content-img-fixed');
+			let imagesCount   = images.length;
+			let imagesPoint   = new Array();
 
-			window.addEventListener('scroll', animateOnScroll);
+			function imagesPoints(){
+				imagesPoint[0] = startPoint;
 
-			function animateOnScroll(params){
-				
-				console.log('pageOffset ' + pageYOffset);
-				console.log('startPoint ' + startPoint);
+				let prevInc = 0;
 
-				function fixed_block(){
+				for(let i = 1; i <= imagesCount; i++){
+					prevInc        = i - 1;
+					imagesPoint[i] =  imagesPoint[prevInc] + winHeight;
+				}
+			}
+
+			imagesPoints();
+
+
+			function change_img(){
+
+				imagesPoint.forEach(function(item, i) {
+					if(pageYOffset > item && pageYOffset < stopPoint){
+						images.removeClass('visible');
+						images.eq(i).addClass('visible');
+					}
+				});
+			}
+
+			function fixed_block(){
 					if(pageYOffset > startPoint && pageYOffset < stopPoint){
 						target.addClass('fixed_top').removeClass('absolute_bottom');
 					} 
@@ -50,63 +55,11 @@
 					}
 				}
 
+
+			window.addEventListener('scroll', animateOnScroll);
+
+			function animateOnScroll(params){
 				fixed_block();
-
-
-				function change_img(){
-
-					if(pageYOffset > image1 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(0).addClass('visible');
-					}
-
-					if(pageYOffset > image2 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(1).addClass('visible');
-					}
-
-					if(pageYOffset > image3 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(2).addClass('visible');
-					}
-
-					if(pageYOffset > image4 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(3).addClass('visible');
-					}
-
-					if(pageYOffset > image5 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(4).addClass('visible');
-					}
-
-					if(pageYOffset > image6 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(5).addClass('visible');
-					}
-
-					if(pageYOffset > image7 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(6).addClass('visible');
-					}
-
-					if(pageYOffset > image8 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(7).addClass('visible');
-					}
-
-					if(pageYOffset > image9 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(8).addClass('visible');
-					}
-
-					if(pageYOffset > image10 && pageYOffset < stopPoint){
-						images.removeClass('visible');
-						images.eq(9).addClass('visible');
-					}
-
-				}
-
 				change_img();
 			}
 
